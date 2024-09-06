42a5736c5c1d6da7b1792401a0d1623dc8616448
Florencia <florenciauehara@gmail.com>
2024-09-06 10:43:43 -0300

    profile

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+import profilePicture from './images/profilephoto.png';
 
 const About = () => {
     return (
@@ -8,6 +9,7 @@ const About = () => {
                 <div className='col-2'>
                     <h2>Acerca de mí</h2>
                     <span className='line'></span>
+                    <img src={profilePicture} alt='Profile' className='profile-picture' />
                     <p>Soy Florencia Uehara, una nueva desarrolladora frontend con ganas de crear experiencias web increíbles. Anteriormente estuve en laboratorios relacionados con la química y microbiología, pero decidí que era momento de entrar al mundo de la programación que tanto me daba curiosidad, ¡y la verdad es que me viene gustando mucho!</p>
                     <p>Cuento con certificado del curso de Desarrollo Frontend, en donde aprendí <b>HTML</b>, <b>CSS</b>, <b>Bootstrap</b>, <b>JavaScript</b>. Además, cuento con certificados en <b>ReactJS</b>, Bases de datos y <b>SQL</b> con PostgreSQL, y <b>Python</b>, mientras realizo el curso del Backend, desarrollando con <b>Django</b>. También tengo conocimiento básico del framework <b>Qwik</b>.</p>
                 </div>
